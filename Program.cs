1aca58a100224b02e8b4769caaa58a6d115fd822
Romanhardy99 <kulenergystreet@mail.ru>
2024-10-08 17:51:25 +0400

    add Logger

--- Program.cs
+++ Program.cs
@@ -10,27 +10,32 @@ using static System.Net.Mime.MediaTypeNames;
 using System.Configuration;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types.Enums;
+using Microsoft.Extensions.Logging;
+using NLog;
+
 
 namespace Main
 {
-  internal class Program 
-  {
-    //public Program() { }
-    //private static List<Task> AllTask;
-    //private static List<Task> MyTask;
-    static void Main(string[] args)
+    internal class Program
     {
-      Host OTask_Manager_bot = new Host();
-      OTask_Manager_bot.Start();
-      //OTask_Manager_bot.OnMessage += OnMessage;
-      Console.ReadLine();
-    }
-    
-    //private static async void OnMessage(ITelegramBotClient client, Update update)
-    //{
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        //public Program() { }
+        //private static List<Task> AllTask;
+        //private static List<Task> MyTask;
+        static void Main(string[] args)
+        {
+            logger.Info("Приложение запущено");
+            Host OTask_Manager_bot = new Host();
+            OTask_Manager_bot.Start();
+            ////OTask_Manager_bot.OnMessage += OnMessage;
+            Console.ReadLine();
+        }
 
-    //}
-  }
+        //private static async void OnMessage(ITelegramBotClient client, Update update)
+        //{
+
+        //}
+    }
 }
 
 
